ce4b47ed028a29198a66ecc414fff42bd1079c1c
Breazu Adrian <breazuadrian@gmail.com>
2016-05-18 10:34:30 +0000

    postinstall.sh edited online with Bitbucket

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# options
+echo "Should I get also the git repositories ?"
+read getrepostiories
+
 # add repositories
 #reboot device
 echo "------------------------"
@@ -18,8 +22,8 @@ wget https://download.jetbrains.com/python/pycharm-community-2016.1.3.tar.gz
 echo "---------------------------"
 echo "--- Basic system update ---"
 echo "---------------------------"
-sudo apt-get --yes --force-yes update
-sudo apt-get --yes --force-yes upgrade
+sudo apt-get --yes --allow update
+sudo apt-get --yes --allow upgrade
 
 
 
@@ -29,18 +33,20 @@ echo "--- Install apps ---"
 echo "--------------------"
 cd ~
 #sudo apt-get install --yes --force-yes sublime-text-installer
-sudo apt-get install --yes --force-yes git vlc vim mc ssh 
+sudo apt-get install --yes --allow git vlc vim mc ssh 
 
 ## install arduino
-sudo apt-get install --yes --force-yes arduino arduino-core 
+sudo apt-get install --yes --allow arduino arduino-core 
 
 ## install python apps
-sudo apt-get install --yes --force-yes python-pip python3-pip web.py python-dev python3-dev apache2 libapache2-mod-wsgi libpq-dev postgresql postgresql-contrib sqlite3 libsqlite3-dev openjdk-9-jre-headless 
-sudo apt-get install --yes --force-yes python-qt4 python-qt4-dbus python-qt4-dev python-qt4-doc python-qt4-gl python-qt4-phonon python-qt4-sql python-qtmobility python-qwt3d-qt4 python-qwt5-qt4 qt4-dev-tools python3-dbus.mainloop.qt python3-pyqt4 python3-pyqt4.phononpython3-pyqt4.qsci python3-pyqt4.qtopengl python3-pyqt4.qtsql 
-
-sudo pip install python-rpi.gpio flask virtualenv RPi.GPIO psycopg2
-sudo pip3 install python3-rpi.gpio django flask virtualenv RPi.GPIO psycopg2
+sudo apt-get install --yes --allow python3-pip web.py python-dev python3-dev apache2 libapache2-mod-wsgi libpq-dev postgresql postgresql-contrib sqlite3 libsqlite3-dev openjdk-9-jre-headless 
+sudo apt-get install --yes --allow python-qt4 python-qt4-dbus python-qt4-dev python-qt4-doc python-qt4-gl python-qt4-phonon python-qt4-sql python-qtmobility python-qwt3d-qt4 python-qwt5-qt4 qt4-dev-tools python3-dbus.mainloop.qt python3-pyqt4 python3-pyqt4.phononpython3-pyqt4.qsci python3-pyqt4.qtopengl python3-pyqt4.qtsql 
+sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
+sudo pip install flask virtualenv RPi.GPIO psycopg2
+sudo pip3 install django flask virtualenv RPi.GPIO psycopg2
 #sudo dpkg -i google-chrome-stable_current_amd64.deb; sudo apt-get -f install
+cd ~/Downloads
 sudo dpkg -i ~/Downloads/*.deb
 sudo mkdir /opt/pycharm.2016.1.3
 sudo tar -xzvf pycharm-community-2016.1.3.tar.gz -C /opt/pycharm.2016.1.3
@@ -62,16 +68,19 @@ echo "------------------------"
 cd ~/repositories
 git config --global user.email "breazuadrian@gmail.com"
 git config --global user.name "Adrian Breazu"
-git clone https://github.com/adrianbreazu/ProjectMoneyBox.git
-git clone https://github.com/adrianbreazu/PyHub.git
-git clone https://github.com/adrianbreazu/Arduino_ultrasonic_car.git
-git clone https://github.com/adrianbreazu/homeautomation.git
-git clone https://github.com/adrianbreazu/LearnDjango19.git
-git clone https://abreazu@bitbucket.org/abreazu/ubuntu_script.git
-git clone https://abreazu@bitbucket.org/abreazu/sprinklerpi.git
-git clone https://bitbucket.org/abreazu/home-automation
-git clone https://bitbucket.org/abreazu/bruno-home
-git clone https://abreazu@bitbucket.org/abreazu/web_pages_tests.git
+if [$getrepostiories = "yes"]
+	then 
+		git clone https://github.com/adrianbreazu/ProjectMoneyBox.git
+		git clone https://github.com/adrianbreazu/PyHub.git
+		git clone https://github.com/adrianbreazu/Arduino_ultrasonic_car.git
+		git clone https://github.com/adrianbreazu/homeautomation.git
+		git clone https://github.com/adrianbreazu/LearnDjango19.git
+		git clone https://abreazu@bitbucket.org/abreazu/ubuntu_script.git
+		git clone https://abreazu@bitbucket.org/abreazu/sprinklerpi.git
+		git clone https://bitbucket.org/abreazu/home-automation
+		git clone https://bitbucket.org/abreazu/bruno-home
+		git clone https://abreazu@bitbucket.org/abreazu/web_pages_tests.git
+fi
 
 
 
